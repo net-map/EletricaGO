733612397f06b7ab183fa24f1c28b8c8feede7d1
Adriano Dennanni <adrianodennanni@gmail.com>
2016-12-11 18:31:58 -0200

    Gifs are in assets folder, images fitted to center of ImageView

--- app/src/main/java/map/net/eletricago/fragments/CameraFragment.java
+++ app/src/main/java/map/net/eletricago/fragments/CameraFragment.java
@@ -1,18 +1,20 @@
 package map.net.eletricago.fragments;
 
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import eu.livotov.labs.android.camview.CameraLiveView;
 import map.net.eletricago.R;
+import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
-import pl.droidsonroids.gif.InputSource;
 
 /**
  * This fragment shows the camera view
@@ -39,13 +41,21 @@ public class CameraFragment extends android.app.Fragment {
 
         mCamera.startCamera();
 
-        setPokemonGif("a");
+        setPokemonGif("pikachu");
 
         return view;
     }
 
     void setPokemonGif(String pokemonName){
-        mPokemonGifImageView.setImageResource(R.drawable.pikachu);
+
+        GifDrawable gifFromAssets = null;
+        try {
+            gifFromAssets = new GifDrawable( getResources().getAssets(), "pokemon_big/"+pokemonName+".gif");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mPokemonGifImageView.setImageDrawable(gifFromAssets);
     }
 
 

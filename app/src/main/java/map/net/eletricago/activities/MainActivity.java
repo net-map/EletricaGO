8596920ee9ed95324f4af83c851997a9a98ee68c
Adriano Dennanni <adrianodennanni@gmail.com>
2016-12-15 00:29:26 -0200

    SplashScreen

--- app/src/main/java/map/net/eletricago/activities/MainActivity.java
+++ app/src/main/java/map/net/eletricago/activities/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
--- app/src/main/java/map/net/eletricago/activities/SplashActivity.java
+++ app/src/main/java/map/net/eletricago/activities/SplashActivity.java
@@ -30,7 +30,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
         Dexter.initialize(this);
 
         View decorView = getWindow().getDecorView();
